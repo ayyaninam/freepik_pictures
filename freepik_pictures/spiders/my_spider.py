031274d9571b43b1480febea0d7ed6308d44cd48
Ayyan Inam <ayaaninam555@gmail.com>
2025-01-08 05:36:02 +0500

    New commit

--- freepik_pictures/spiders/my_spider.py
+++ freepik_pictures/spiders/my_spider.py
@@ -181,18 +181,6 @@ class MySpider(scrapy.Spider):
     def start_requests(self):
         # Add proper headers
         self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-            'Accept': 'application/json, text/plain, */*',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Referer': 'https://www.freepik.com/',
-            'Origin': 'https://www.freepik.com',
-            'Connection': 'keep-alive',
-            'Cache-Control': 'no-cache',
-            'Pragma': 'no-cache',
-            'DNT': '1',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-origin',
         }
 
         # Add cookies if you have them
